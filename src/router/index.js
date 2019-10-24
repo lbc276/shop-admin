0f474529e8ad68215269fd41b463556c8231c6dd
lbc276 <libeixi0815@163.com>
2019-10-24 16:41:03 +0800

    配置浏览器自动打开

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Index from '../components/Index.vue'
+import Login from '../components/Login.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  { path: '/', redirect: '/index' },
+  { path: '/index', component: Index },
+  { path: '/login', component: Login }
 ]
 
 const router = new VueRouter({
