4281fa90c1a18f0e9a86a201438477d54352b523
lbc276 <libeixi0815@163.com>
2019-10-29 19:56:43 +0800

    首页左侧导航渲染完成

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -23,32 +23,19 @@
         <el-menu
           router
           unique-opened
+          :default-active="defaultActive"
           background-color="#545c64"
           text-color="#fff"
           active-text-color="#ffd04b"
         >
-          <el-submenu index="1">
+          <el-submenu :index="menu.path" v-for="menu in menuList" :key="menu.id">
             <template v-slot:title>
               <i class="el-icon-location"></i>
-              <span>用户管理</span>
+              <span>{{menu.authName}}</span>
             </template>
-            <el-menu-item index="users">
+            <el-menu-item v-for="item in menu.children" :key="item.id" :index="item.path">
               <i class="el-icon-menu"></i>
-              <span slot="title">用户列表</span>
-            </el-menu-item>
-          </el-submenu>
-          <el-submenu index="2">
-            <template slot="title">
-              <i class="el-icon-location"></i>
-              <span>权限管理</span>
-            </template>
-            <el-menu-item index="roles">
-              <i class="el-icon-menu"></i>
-              <span slot="title">角色列表</span>
-            </el-menu-item>
-            <el-menu-item index="rights">
-              <i class="el-icon-menu"></i>
-              <span slot="title">权限管理</span>
+              <span slot="title">{{item.authName}}</span>
             </el-menu-item>
           </el-submenu>
         </el-menu>
@@ -62,6 +49,22 @@
 
 <script>
 export default {
+  computed: {
+    defaultActive () {
+      return this.$route.path.slice(1)
+    }
+  },
+  async created () {
+    const { meta, data } = await this.$axios.get('menus')
+    if (meta.status === 200) {
+      this.menuList = data
+    }
+  },
+  data () {
+    return {
+      menuList: []
+    }
+  },
   methods: {
     logout () {
       this.$confirm('确定要退出本系统吗?', '温馨提示', {
